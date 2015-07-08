edbe15797832a46f03b22960c8ea2696a8a7dc65
David Cournapeau <cournape@gmail.com>
2015-07-08 14:30:53 +0100

    MAINT: start 0.0.9.

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ from setuptools import setup
 
 MAJOR = 0
 MINOR = 0
-MICRO = 8
+MICRO = 9
 
-IS_RELEASED = True
+IS_RELEASED = False
 
 VERSION_INFO = (MAJOR, MINOR, MICRO)
 VERSION = ".".join(str(i) for i in VERSION_INFO)
