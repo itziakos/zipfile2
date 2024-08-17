9b3851bd77428de7e07764dd8e420634c22873c1
Ioannis Tziakos <mail@itziakos.gr>
2024-08-17 11:06:16 +0100

    Fix bad copypasta

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 HERE = Path(__file__).parent
 version = (HERE / 'VERSION').read_text().strip()
-filename = (HERE / 'win32ctypes' / 'version.py').write_text(f'__version__ = "{version}"\n')
+filename = (HERE / 'zipfile2' / '_version.py').write_text(f'__version__ = "{version}"\n')
 
 setup()
