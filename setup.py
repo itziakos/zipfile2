893e4b2cf4649463e4f6f59e5077bdfb937d8f3e
Ioannis Tziakos <mail@itziakos.gr>
2024-08-17 11:44:28 +0100

    Minor cleanup

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup
 
 HERE = Path(__file__).parent
 version = (HERE / 'VERSION').read_text().strip()
-filename = (
-    HERE / 'zipfile2' / '_version.py').write_text(
-        f'__version__ = "{version}"\n')
+filename = (HERE / 'zipfile2' / '_version.py')
+filename.write_text(f"__version__ = '{version}'\n")
 
 setup()
